ae4975bf6afc0c647a8f333ec3fb9d3fa64834f8
Bayu Setiawan <bayusetiawanipb@gmail.com>
2024-05-27 14:08:43 +0700

    feat(career): greet text

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,10 +1,12 @@
 import { getCareers } from '@/data-access/careers/get-careers.persistence'
+import Greet from '@/components/elements/greet'
 import CareerTable from '@/components/module/career/career-table'
 
 export default async function CareerPage() {
   const careers = await getCareers()
   return (
-    <div className="flex w-full justify-center">
+    <div className="flex w-full flex-col justify-center">
+      <Greet />
       <CareerTable careers={careers} />
     </div>
   )
